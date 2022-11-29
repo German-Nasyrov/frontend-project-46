9580bd4f3db12ce42df463ed0226858e184beff7
German <german.nasyrov.95@mail.ru>
2022-11-29 08:09:39 +0300

    add index.js with logic

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import { program } from 'commander';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0')
+  .version('0.1.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const diff = gendiff(filepath1, filepath2);
+    console.log(diff);
+  })
   .parse(process.argv);
 
 export default program;
