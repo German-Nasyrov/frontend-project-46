869047bf2d10a7c0ac19d29885999ded1635b03f
German <german.nasyrov.95@mail.ru>
2022-11-29 08:10:09 +0300

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,4 @@
 #!/usr/bin/env node
-import isResultEquals from '../src/games/calc-expression.js';
+import program from '../src/index.js';
 
-isResultEquals();
-
-import { program } from 'commander';
-
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0')
-  .parse(process.argv);
+program();
