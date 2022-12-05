73b8c17e2884fb415da3c39853e212c04e458ef8
German <german.nasyrov.95@mail.ru>
2022-12-05 10:14:24 +0300

    add makeTree.js which makes document structure

---
+++ src/makeTree.js
@@ -0,0 +1,41 @@
+import _ from 'lodash';
+
+const makeTree = (file1, file2) => {
+  const file1Keys = _.keys(file1);
+  const file2Keys = _.keys(file2);
+  const sortedKeys = _.sortBy(_.union(file1Keys, file2Keys));
+
+  return sortedKeys.map((key) => {
+    if (!_.has(file1, key)) {
+      return { key, state: 'added', value: file2[key] };
+    }
+    if (!_.has(file2, key)) {
+      return { key, state: 'deleted', value: file1[key] };
+    }
+    if (_.isObject(file1[key]) && _.isObject(file2[key])) {
+      return { key, state: 'nested', value: makeTree(file1[key], file2[key]) };
+    }
+    if (!_.isEqual(file1[key], file2[key])) {
+      return {
+        key,
+        state: 'changed',
+        value1: file1[key],
+        value2: file2[key],
+      };
+    }
+
+    return { key, state: 'notChanged', value: file1[key] };
+  });
+};
+
+export default makeTree;
+
+console.log(
+  _.sortBy(
+    _.union(_.keys('../__fixtures__/file1.json', _.keys('../__fixtures__/file2.json'))),
+  ),
+);
+
+console.log(
+  JSON.stringify(makeTree('../__fixtures__/file1.json', '../__fixtures__/file2.json')),
+);
