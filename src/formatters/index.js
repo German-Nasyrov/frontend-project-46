7ab36c1b4887ce96cd11aa7dd37ed52af892c815
German <german.nasyrov.95@mail.ru>
2022-12-05 10:13:02 +0300

    add formatters dir with different formats

---
+++ src/formatters/index.js
@@ -0,0 +1,16 @@
+import makePlain from './plain.js';
+import makeStylish from './stylish.js';
+import makeJson from './json.js';
+
+export default (data, format, replacer) => {
+  switch (format) {
+    case 'stylish':
+      return makeStylish(data, replacer);
+    case 'plain':
+      return makePlain(data);
+    case 'json':
+      return makeJson(data, null, replacer);
+    default:
+      throw new Error(`Wrong file format type: '.${format}'! Try other file formats.`);
+  }
+};
