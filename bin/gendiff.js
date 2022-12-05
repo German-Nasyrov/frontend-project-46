9f420b472cf22dbc0700f1fad7caddd9de8c2194
German <german.nasyrov.95@mail.ru>
2022-12-05 10:11:46 +0300

    add gendiff.js with programm menu logic

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
-import program from '../src/index.js';
+import { Command } from 'commander';
+import gendiff from '../src/index.js';
 
-program();
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const difference = gendiff(filepath1, filepath2, program.opts().format);
+    console.log(difference);
+  });
+
+program.parse();
