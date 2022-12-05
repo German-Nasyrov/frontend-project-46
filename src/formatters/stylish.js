6f88db7502fe5ae5c470abe08e8bb3627e167f03
German <german.nasyrov.95@mail.ru>
2022-12-05 11:24:03 +0300

    fix eslint errors in plain.js and stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -22,33 +22,33 @@ const sign = {
 
 const makeStylish = (diff, replacer = '    ') => {
   const iter = (tree, depth) => tree.map((node) => {
-      const indent = replacer.repeat(depth);
-      const indentForSign = indent.slice(2);
+    const indent = replacer.repeat(depth);
+    const indentForSign = indent.slice(2);
 
-      const makeLine = (value, mark) => `${indentForSign}${mark} ${node.key}: ${stringify(value, depth, replacer)}`;
+    const makeLine = (value, mark) => `${indentForSign}${mark} ${node.key}: ${stringify(value, depth, replacer)}`;
 
-      switch (node.state) {
-        case 'added':
-          return makeLine(node.value, sign.added);
-        case 'deleted':
-          return makeLine(node.value, sign.deleted);
-        case 'notChanged':
-          return makeLine(node.value, sign.unchanged);
-        case 'changed':
-          return [
-            `${makeLine(node.value1, sign.deleted)}`,
-            `${makeLine(node.value2, sign.added)}`,
-          ].join('\n');
-        case 'nested':
-          return `${indent}${node.key}: ${[
-            '{',
-            ...iter(node.value, depth + 1),
-            `${indent}}`,
-          ].join('\n')}`;
-        default:
-          throw new Error(`Type: ${node.state} is undefined`);
-      }
-    });
+    switch (node.state) {
+      case 'added':
+        return makeLine(node.value, sign.added);
+      case 'deleted':
+        return makeLine(node.value, sign.deleted);
+      case 'notChanged':
+        return makeLine(node.value, sign.unchanged);
+      case 'changed':
+        return [
+          `${makeLine(node.value1, sign.deleted)}`,
+          `${makeLine(node.value2, sign.added)}`,
+        ].join('\n');
+      case 'nested':
+        return `${indent}${node.key}: ${[
+          '{',
+          ...iter(node.value, depth + 1),
+          `${indent}}`,
+        ].join('\n')}`;
+      default:
+        throw new Error(`Type: ${node.state} is undefined`);
+    }
+  });
 
   const stylishDiff = iter(diff, 1);
   return ['{', ...stylishDiff, '}'].join('\n');
