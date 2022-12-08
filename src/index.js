1c22c9e2e5383fedade201ba435e7147a23d21a1
German <german.nasyrov.95@mail.ru>
2022-12-08 16:38:16 +0300

    rename file variables to data

--- src/index.js
+++ src/index.js
@@ -4,22 +4,21 @@ import makeTree from './makeTree.js';
 import parse from './parsers.js';
 import formatOutput from './formatters/index.js';
 
-// eslint-disable-next-line no-undef
 const getAbsolutePath = (file) => path.resolve(process.cwd(), file);
-const getExtensions = (file) => path.extname(file).slice(1);
+const getFormats = (file) => path.extname(file).slice(1);
 
 const getContentFromFile = (file) => {
   const absolutePath = getAbsolutePath(file);
   const fileContent = fs.readFileSync(absolutePath, 'utf8');
-  const extension = getExtensions(file);
+  const format = getFormats(file);
 
-  return parse(fileContent, extension);
+  return parse(fileContent, format);
 };
 
 const gendiff = (filePath1, filePath2, format = 'stylish') => {
-  const file1 = getContentFromFile(filePath1);
-  const file2 = getContentFromFile(filePath2);
-  const diffInfo = makeTree(file1, file2);
+  const data1 = getContentFromFile(filePath1);
+  const data2 = getContentFromFile(filePath2);
+  const diffInfo = makeTree(data1, data2);
   const formattedTree = formatOutput(diffInfo, format);
 
   return formattedTree;
