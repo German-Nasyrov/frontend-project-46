32b21256e91b6e4011232a0a82a15ce2e6b6dabd
German <german.nasyrov.95@mail.ru>
2022-12-08 16:43:52 +0300

    rename extension param to format

--- src/parsers.js
+++ src/parsers.js
@@ -1,13 +1,13 @@
 import yaml from 'js-yaml';
 
-export default (fileContent, extension) => {
-  switch (extension) {
+export default (fileContent, format) => {
+  switch (format) {
     case 'json':
       return JSON.parse(fileContent);
     case 'yml':
     case 'yaml':
       return yaml.load(fileContent);
     default:
-      throw new Error(`Wrong file extension: '${extension}'! Try other formats.`);
+      throw new Error(`Wrong file extension: '${format}'! Try other formats.`);
   }
 };
