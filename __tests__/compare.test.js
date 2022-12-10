5417375d5eb364967388ec7d6d658b87d590fa34
German <german.nasyrov.95@mail.ru>
2022-12-10 16:31:20 +0300

    fix build error

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -7,23 +7,25 @@ import gendiff from '../src/index.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
-
 const expectedStylish = readFile('correct-stylish.txt');
 const expectedPlain = readFile('correct-plain.txt');
 const expectedJson = readFile('correct-json.txt');
-const expectedResults = [expectedStylish, expectedPlain, expectedJson];
 const formats = ['json', 'yaml', 'yml'];
 const formatters = ['stylish', 'plain', 'json'];
+const chooseExpectedResult = (value) => {
+  if (value === 'stylish') return expectedStylish;
+  if (value === 'plain') return expectedPlain;
+  if (value === 'json') return expectedJson;
+  return 'Unexpected error';
+};
 
 describe('Positives cases', () => {
   describe.each(formats)('Format %s', (format) => {
     const file1 = `${process.cwd()}/__fixtures__/file1.${format}`;
     const file2 = `${process.cwd()}/__fixtures__/file2.${format}`;
-    let counter = 0;
 
     test.each(formatters)('Check correct %s', (formatter) => {
-      expect(gendiff(file1, file2, formatter)).toEqual(expectedResults[counter]);
-      counter += 1;
+      expect(gendiff(file1, file2, formatter)).toEqual(chooseExpectedResult(formatter));
     });
   });
 });
