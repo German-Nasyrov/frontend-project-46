02a5cdfe7a57305a5414d35e360f0afb284c5b38
German <german.nasyrov.95@mail.ru>
2022-12-10 14:47:55 +0300

    add test.each in describe.each construction

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -11,29 +11,26 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8')
 const expectedStylish = readFile('correct-stylish.txt');
 const expectedPlain = readFile('correct-plain.txt');
 const expectedJson = readFile('correct-json.txt');
-
+const expectedResults = [expectedStylish, expectedPlain, expectedJson];
 const formats = ['json', 'yaml', 'yml'];
+const formatters = ['stylish', 'plain', 'json'];
 
 describe('Positives cases', () => {
   describe.each(formats)('Format %s', (format) => {
     const file1 = `${process.cwd()}/__fixtures__/file1.${format}`;
     const file2 = `${process.cwd()}/__fixtures__/file2.${format}`;
+    let counter = 0;
 
-    test('Correct stylish', () => {
-      expect(gendiff(file1, file2, 'stylish')).toEqual(expectedStylish);
-    });
-    test('Correct plain', () => {
-      expect(gendiff(file1, file2, 'plain')).toEqual(expectedPlain);
-    });
-    test('Correct json', () => {
-      expect(gendiff(file1, file2, 'json')).toEqual(expectedJson);
+    test.each(formatters)('Check correct %s', (formatter) => {
+      expect(gendiff(file1, file2, formatter)).toEqual(expectedResults[counter]);
+      counter += 1;
     });
   });
 });
 
 describe('Negative cases', () => {
-  test('Check wrong file extension', () => {
-    const error = new Error("Wrong file extension: 'txt'! Try other formats.");
+  test('Check wrong format', () => {
+    const error = new Error('Something went wrong, try again!');
 
     expect(() => {
       gendiff(getFixturePath('file1-wrong.txt'), getFixturePath('file2-wrong.txt'));
